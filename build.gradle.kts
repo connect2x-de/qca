3bb5bce58f95af116bdaed100eb920d3c24b655c
benkuly <kontakt@folivo.net>
2024-05-14 14:52:16 +0200

    try dokka frist

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
     `maven-publish`
     signing
+    alias(libs.plugins.dokka)
     alias(libs.plugins.android.library).apply(false)
     alias(libs.plugins.kotlin.multiplatform).apply(false)
     alias(libs.plugins.kotlinx.serialization).apply(false)
-    alias(libs.plugins.dokka)
 }
 
 allprojects {
